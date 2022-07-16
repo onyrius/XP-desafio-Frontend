276e722c11d4bb73086bc92228c255ec65c64dd7
Suelen Arruda <suelenonyrius@gmail.com>
2022-07-16 19:49:56 -0300

    feat: :sparkles: adiciona componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function Header() {
+  const user = useSelector(({ login: { email } }) => email);
+  console.log(user);
+  return (
+    <header className="header">
+      <h3 className="header-title">
+        {' '}
+        usuário:
+        {' '}
+        { user }
+      </h3>
+    </header>
+  );
+}
+
+export default Header;
