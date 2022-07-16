89043768def4638d13415ef5259503f9accb0a62
Suelen Arruda <suelenonyrius@gmail.com>
2022-07-16 19:51:26 -0300

    feat: :sparkles:
    cria pagina Saldo

---
+++ src/pages/Saldo.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+function Saldo() {
+  return (
+
+    <section className="Saldo">
+      <Header />
+      <section className="meu-Saldo">
+        ola
+      </section>
+      <Footer />
+    </section>
+
+  );
+}
+
+export default Saldo;
