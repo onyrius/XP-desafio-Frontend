26336b59f34d44f0ace6a444b5832019052053bb
Suelen Arruda <suelenonyrius@gmail.com>
2022-07-16 19:47:09 -0300

    feat: :sparkles: cria pasta helpers para futura refatoração

---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+// export const handleChange = ({ target }) => {
+//   const { name, value } = target;
+
+//   // console.log(name, value);
+//   setUserLogin((prevState) => ({
+//     ...prevState,
+//     [name]: value,
+//   }));
+// };
