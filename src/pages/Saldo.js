e284f9db54635398730af95d2141ceebc047ac8f
Suelen Arruda <suelenonyrius@gmail.com>
2022-07-16 22:55:55 -0300

    feat: adiciona rota Saldo

--- src/pages/Saldo.js
+++ src/pages/Saldo.js
@@ -2,14 +2,15 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import SaldoTotal from '../components/SaldoTotal';
 
 function Saldo() {
   return (
 
-    <section className="Saldo">
+    <section className="Acoes">
       <Header />
       <section className="meu-Saldo">
-        ola
+        <SaldoTotal />
       </section>
       <Footer />
     </section>
