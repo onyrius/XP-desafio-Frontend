b062c4f17fc6db6957d8e93b938484e15fd66cc6
Suelen Arruda <suelenonyrius@gmail.com>
2022-07-16 22:54:51 -0300

    feat: :sparkles: adiciona pagina Acoes

--- src/pages/Acoes.js
+++ src/pages/Acoes.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-console */
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import BolsaAcoes from '../components/BolsaAcoes';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import MinhasAcoes from '../components/MinhasAcoes';
 
 function Acoes() {
+  const navigate = useNavigate();
+  const redirecionar = () => {
+    navigate('/');
+  };
   return (
 
     <section className="Acoes">
@@ -15,6 +20,13 @@ function Acoes() {
         <BolsaAcoes />
       </section>
       <Footer />
+      <button
+        className="footer"
+        type="button"
+        onClick={redirecionar}
+      >
+        Sair
+      </button>
     </section>
 
   );
