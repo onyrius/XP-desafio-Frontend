3c120d7727487dab888e88ba893a0af3ce3cd860
Suelen Arruda <suelenonyrius@gmail.com>
2022-07-18 10:04:52 -0300

    feat: :sparkles: adiciona nome do usuario ao componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function Header() {
-  const user = useSelector(({ login: { email } }) => email);
+  const nomeDoUsuario = useSelector(({ usuarioLogado: { nome } }) => nome);
   return (
     <header className="header">
       <h3 className="header-title">
-        {`usuário: ${user}` }
+        {`usuário: ${nomeDoUsuario}` }
       </h3>
     </header>
   );
