dd00e1e4c7be07fd22e71cc5ca00e0381bb59a39
Suelen Arruda <suelenonyrius@gmail.com>
2022-07-23 19:58:31 -0300

    feat: :sparkles: adiciona toast wrongmail, sucessLogin , digiteUmValorValido, saldoInsuficiente e verificaAcaoVender aos helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,99 @@
+/* eslint-disable consistent-return */
 import toast from 'react-hot-toast';
 
-export const wrongEmail = () => toast.error('Email inválido!');
-export const sucessLogin = () => toast.success('Login realizado com sucesso!');
+export const wrongEmail = () => toast.error(
+  'Email inválido!',
+  {
+    icon: '⛔',
+    style: {
+      borderRadius: '10px',
+      background: '#333',
+      color: '#fff',
+    },
+  },
+);
+export const sucessLogin = () => toast.success(
+  'Olá!',
+  {
+    icon: '👏',
+    style: {
+      borderRadius: '10px',
+      background: '#333',
+      color: '#fff',
+    },
+  },
+);
+
+export const digiteUmValorValido = () => toast(
+  'Digite um valor válido',
+  {
+    icon: '⛔',
+    style: {
+      borderRadius: '10px',
+      background: '#333',
+      color: '#fff',
+    },
+  },
+);
+
+export const saldoInsuficiente = () => toast(
+  'saldo insuficiente',
+  {
+    icon: '⛔',
+    style: {
+      borderRadius: '10px',
+      background: '#333',
+      color: '#fff',
+    },
+  },
+);
+
+export const verificaAcao = (
+  ativosBolsa,
+  ativoEscolhido,
+  quantidadeDigitada,
+  setRenderAtivo,
+) => {
+  const ativoEscolhidoBolsa = ativosBolsa
+    .find((ativoBolsa) => ativoBolsa.acao === ativoEscolhido.acao);
+
+  if (!quantidadeDigitada.length || !ativoEscolhido) {
+    digiteUmValorValido();
+  }
+  if (+quantidadeDigitada > +ativoEscolhidoBolsa.qntdade) {
+    digiteUmValorValido();
+  }
+  if (+quantidadeDigitada <= +ativoEscolhidoBolsa.qntdade) {
+    setRenderAtivo((prev) => ({
+      ...prev,
+      qntdade: Number(prev.qntdade) + Number(quantidadeDigitada),
+    }));
+    return false;
+  }
+  return true;
+};
+
+export const verificaAcaoVender = (
+  ativoEscolhido,
+  quantidadeDigitada,
+  setRenderAtivo,
+) => {
+  if (!quantidadeDigitada.length || !ativoEscolhido.qntdade) {
+    console.log('dentro do primeiro if');
+    digiteUmValorValido();
+  }
+  if (Number(quantidadeDigitada) > +ativoEscolhido.qntdade) {
+    console.log('dentro do segundo if');
+
+    digiteUmValorValido();
+  }
+  if (Number(quantidadeDigitada) <= +ativoEscolhido.qntdade) {
+    console.log(+ativoEscolhido.qntdade);
+    setRenderAtivo((prev) => ({
+      ...prev,
+      qntdade: Number(prev.qntdade) - Number(quantidadeDigitada),
+    }));
+    return false;
+  }
+  return true;
+};
